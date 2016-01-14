11b87cd678e7509f6bfa44bd0a20364a4e4824eb
hexl <hexlmail@163.com>
2016-01-14 17:31:45 +0800

    sd

--- src/main/java/com/pyt/facade/user/service/impl/DemoServiceImpl.java
+++ src/main/java/com/pyt/facade/user/service/impl/DemoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.pyt.facade.user.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.pyt.facade.user.service.DemoService;
 
+@Service("demoService")
 public class DemoServiceImpl implements DemoService {
 	 
     public String sayHello(String name) {
